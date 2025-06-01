fdf7c8d603f7b6f91df240edbef53dc90573e8cb
Pranav Rana <ranapranav333@gmail.com>
2025-06-01 17:21:43 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html", "./index.css"],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'class', // Enable dark mode via a class
+  plugins: [],
+}
